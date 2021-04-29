5683f61817e622109d0314f7fa89eeef0daaf8ef
yeunyong <yeunyong.ka@gmail.com>
2021-04-29 16:47:10 +0700

    .gitignore fix

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Product } from '../models/product';
+import { ProductItemComponent } from '../product-item/product-item.component';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  @ViewChildren(ProductItemComponent)
+  productItems: QueryList<ProductItemComponent>;
+
+  products: Product[];
+  
+  constructor() { 
+    this.products = [];
+  }
+
+  ngOnInit(): void {
+  }
+  
+  selectedProduct(productComponent: ProductItemComponent) {
+    alert('Product ${productComponent.product.name} selected');
+    this.productItems.forEach(p => {
+      p.isSelected = false;
+    });
+    productComponent.isSelected = true;
+    }
+  }
